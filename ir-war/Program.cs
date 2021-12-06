b71812267f7c2f26f46ced93a4ef4cb3ca3a46d3
Artur Kashuba <40093366+Rockstrip@users.noreply.github.com>
2021-12-06 21:05:20 +0200

    init

---
+++ ir-war/Program.cs
@@ -0,0 +1,6 @@
+using ir_war;
+
+public static class Program
+{
+    public static void Main(string[] args) => new Server().Start();
+}
---
+++ ir-war/Server.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace ir_war
+{
+    public class Server
+    {
+        private TcpListener tcplistener;
+        private Thread listenThread;
+        private ConcurrentBag<NetworkStream> networkStreams;
+        private event Action<string> OnDataReceived;
+
+        public Server()
+        {
+            tcplistener = new TcpListener(IPAddress.Any, 8001);
+            listenThread = new Thread(ListenForClients);
+            networkStreams = new ConcurrentBag<NetworkStream>();
+            OnDataReceived += SendToAllClients;
+        }
+        public void Start() => listenThread.Start();
+        private void ListenForClients()
+        {
+            tcplistener.Start();
+            while (true)
+            {
+                TcpClient client = tcplistener.AcceptTcpClient();
+
+                Thread clientThread = new Thread(ClientProcess);
+                clientThread.Start(client);
+            }
+        }
+        private void ClientProcess(object? client)
+        {
+            TcpClient tcpClient = (TcpClient) client!;
+            NetworkStream clientStream = tcpClient.GetStream();
+            networkStreams.Add(clientStream);
+
+            while (true)
+            {
+                try
+                {
+                    if (clientStream.CanRead)
+                    {
+                        byte[] bytes = new byte[2];
+                        clientStream.Read(bytes, 0, 2);
+
+                        string returnData = Encoding.UTF8.GetString(bytes);
+                        OnDataReceived.Invoke(returnData);
+                        Console.WriteLine(returnData);
+                    }
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine(e.Message);
+                    break;
+                }
+            }
+        }
+        private void SendToAllClients(string data)
+        {
+            foreach (var stream in networkStreams)
+            {
+                if (stream.CanWrite)
+                {
+                    var mes = Encoding.UTF8.GetBytes(data);
+                    stream.Write(mes, 0, mes.Length);
+                }
+            }
+        }
+    }
+}
